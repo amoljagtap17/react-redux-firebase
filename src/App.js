8ac5a3cff405e58d770cd00168d2e55accd9bda6
Amol Jagtap <amoljagtap17@gmail.com>
2019-10-22 00:28:58 +0530

    REACT-REDUX-FIREBASE: Initial component files and add react-router-dom

--- src/App.js
+++ src/App.js
@@ -1,7 +1,12 @@
 import React from 'react'
+import { BrowserRouter as Router } from 'react-router-dom'
 
 function App() {
-  return <div>React App </div>
+  return (
+    <Router>
+      <div>React App </div>
+    </Router>
+  )
 }
 
 export default App
