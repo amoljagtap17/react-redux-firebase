319c1a63b5d8fa09c900f50870c55c7bf09a57d3
Amol Jagtap <amoljagtap17@gmail.com>
2019-10-22 00:41:31 +0530

    REACT-REDUX-FIREBASE: Add Navbar component logic

--- src/App.js
+++ src/App.js
@@ -1,9 +1,11 @@
 import React from 'react'
 import { BrowserRouter as Router } from 'react-router-dom'
+import Navbar from './components/layout/Navbar'
 
 function App() {
   return (
     <Router>
+      <Navbar />
       <div>React App </div>
     </Router>
   )
