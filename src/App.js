050cec7939588baf7109acd5eb0e69edaba2abe5
Amol Jagtap <amoljagtap17@gmail.com>
2019-10-22 01:09:06 +0530

    REACT-REDUX-FIREBASE: Add ProjectDetails component

--- src/App.js
+++ src/App.js
@@ -2,13 +2,15 @@ import React from 'react'
 import { BrowserRouter as Router, Switch, Route } from 'react-router-dom'
 import Navbar from './components/layout/Navbar'
 import Dashboard from './components/dashboard/Dashboard'
+import ProjectDetails from './components/projects/ProjectDetails'
 
 function App() {
   return (
     <Router>
       <Navbar />
       <Switch>
-        <Route path="/" component={Dashboard} />
+        <Route exact path="/" component={Dashboard} />
+        <Route path="/project/:id" component={ProjectDetails} />
       </Switch>
     </Router>
   )
